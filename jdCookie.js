ebd1197972505c3f319b3affa5511519dbdd9f6c
Young Lee <ncisoft@hotmail.com>
2022-07-06 17:55:00 +0800

    lmde: fixed sendNotify.js

--- jdCookie.js
+++ jdCookie.js
@@ -12,31 +12,8 @@
  */
 //此处填写京东账号cookie。
 const { readFileSync, existsSync } = require("fs")
+const { getEnv  } = require("./getEnv");
 
-function getEnv(name) {
-    if (!name) return null;
-
-    const envFile = "/ql/scripts/env.json"
-    if (!existsSync(envFile)) {
-      console.log('读取配置文件失败!!!:', envFile);
-      return null;
-    }
-
-    try {
-        const env = JSON.parse(readFileSync(envFile, 'utf-8'))
-        let val = env[name] || env[name.toLowerCase()] || env[name.toUpperCase()];
-        if (val) val = val.trim()
-        console.log(`读取配置文件成功: ${envFile}`)
-        return val;
-    } catch (error) {
-        console.log('读取配置文件失败:', error)
-        return null;
-    }
-
-    const fromEnv = process.env[name] || process.env[name.toLowerCase()] || process.env[name.toUpperCase()]
-    if (fromEnv) return fromEnv.trim();
-
-}
 const jdCookie = getEnv('JD_COOKIE')
 
 let CookieJDs = [
